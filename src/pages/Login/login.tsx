57c47e0327038fd4cdae700b6c0beff4990e8a8a
Cameron Foskett <cameron.foskett@ibm.com>
2022-09-08 08:54:38 +0100

    added blank pages

---
+++ src/pages/Login/login.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import './loginStyles';
+
+export default function Login() {
+    return <Button className="button" variant="contained">Hello World</Button>;
+
+}
+
